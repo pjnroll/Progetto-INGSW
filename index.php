10da958dd708f507f9af0868b6836dbee68b6676
Gabriele <ga.pisciotta@gmail.com>
2017-06-16 17:18:06 +0200

    Inserimento template index.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+// Pagina index della piattaforma. Verifica inizialmente se l'user è loggato, in caso negativo lo forza a loggarsi,
+// altrimenti a seconda del suo ruolo mostra l'area clienti o il pannello di amministrazione.
+?>
+<html>
+	<head>
+		<title>IoT</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/stili.css" rel="stylesheet">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<script src="js/lib/jquery.js"></script>
+		<script src="js/lib/bootstrap.min.js"></script>
+        <script src="js/standard.js"></script>
+    </head>
+	
+	<body>
+		<div id = "maindiv">
+		</div>
+	</body>
+</html>
