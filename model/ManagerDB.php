125a1371a0e18aab4f0a0854b78f117b0f0d67d5
Gabriele <ga.pisciotta@gmail.com>
2017-06-17 15:23:25 +0200

    riformattato

--- model/ManagerDB.php
+++ model/ManagerDB.php
@@ -1,35 +1,33 @@
 <?php
-class ManagerDB {
-    protected $link;
-    private $_db;
-    static $_instance;
+    class ManagerDB {
+        private $_db;
+        static $_instance;
 
+        private function __clone(){}
 
-    private function __clone(){}
-
-    public static function getInstance() {
-        if (!(self::$_instance instanceof self)) {
-            self::$_instance = new self();
+        public static function getInstance() {
+            if (!(self::$_instance instanceof self)) {
+                self::$_instance = new self();
+            }
+            return self::$_instance;
         }
-        return self::$_instance;
-    }
 
-    public function query($sql) {
-        return query($this->_db,$sql);
-    }
-
-    public function __construct($server = "", $username = "", $password = "", $db = "") {
-        $this->_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // Se son stati passi come parametri i dati del db, li uso per connettermi
-        if ($server != "" && $username != "" && $username != "" && password != "" && $db != "") {
-            $this->_db = new PDO('mysql:host='.$server.'; dbname='.$db, $username, $password);
+        public function query($sql) {
+            return query($this->_db,$sql);
         }
-        else {
-            // Altrimenti procedo di default con quelli locali.
-            $this->_db = new PDO('mysql:host=127.0.0.1;dbname=iot', 'root', '');
-        }
-        $this->connect();
-    }
 
-}
+        public function __construct($server = "", $username = "", $password = "", $db = "") {
+            $this->_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Se son stati passi come parametri i dati del db, li uso per connettermi
+            if ($server != "" && $username != "" && $username != "" && password != "" && $db != "") {
+                $this->_db = new PDO('mysql:host='.$server.'; dbname='.$db, $username, $password);
+            }
+            else {
+                // Altrimenti procedo di default con quelli locali.
+                $this->_db = new PDO('mysql:host=127.0.0.1;dbname=iot', 'root', '');
+            }
+            $this->connect();
+        }
+    }
 ?>
