e61d1366db6fb273aaf430aed1c3d1afd9ea6774
Gabriele <ga.pisciotta@gmail.com>
2017-06-18 20:57:06 +0200

    Aggiunto classe Sito

---
+++ model/Sito.php
@@ -0,0 +1,19 @@
+<?php
+
+class Sito
+{
+    private $ID;
+    private $IDCliente;
+    private $Nome;
+    private $Grandezza;
+    private $Località;W
+
+    public function __get($var) {
+        return $this->$var;
+    }
+
+    public function __set($var, $val) {
+        $this->$var = $val;
+    }
+
+}
